40f72412266a6bf61964881df084d83720def303
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-04-09 00:56:30 +0700

    Add quit shortcut

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -5,7 +5,7 @@ extends Control
 
 
 func _ready() -> void:
-	file_menu.add_item("Quit", 0)
+	file_menu.add_item("Quit", 0, KEY_MASK_CTRL | KEY_Q)
 	file_menu.id_pressed.connect(_on_file_menu_id_pressed)
 
 
