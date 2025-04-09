cea7a0eada671bab1cc4ff4732eb04aa244e81b8
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-04-09 20:48:40 +0700

    Optimize Tree Root node

--- graph_nodes/tree_root/tree_root.gd
+++ graph_nodes/tree_root/tree_root.gd
@@ -2,24 +2,22 @@ class_name TreeRoot
 extends GraphNode
 
 
+@onready var seed_spinbox: SpinBox = %SpinBox
+@onready var x_spinbox: SpinBox = %XSpinBox
+@onready var y_spinbox: SpinBox = %YSpinBox
+@onready var z_spinbox: SpinBox = %ZSpinBox
+
+
 var tree_seed: int:
 	get:
-		var spinbox: SpinBox = %SpinBox
-		return int(spinbox.value)
+		return int(seed_spinbox.value)
 	set(value):
-		var spinbox: SpinBox = %SpinBox
-		spinbox.value = value
+		seed_spinbox.value = value
 
 var tree_offset: Vector3:
 	get:
-		var x_spinbox: SpinBox = %XSpinBox
-		var y_spinbox: SpinBox = %YSpinBox
-		var z_spinbox: SpinBox = %ZSpinBox
 		return Vector3(x_spinbox.value, y_spinbox.value, z_spinbox.value)
 	set(value):
-		var x_spinbox: SpinBox = %XSpinBox
-		var y_spinbox: SpinBox = %YSpinBox
-		var z_spinbox: SpinBox = %ZSpinBox
 		x_spinbox.value = value.x
 		y_spinbox.value = value.y
 		z_spinbox.value = value.z
